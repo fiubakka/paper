45845f0853d9a9933e1475466ac2b277c95f190d
Marcos <marcos.rolando@tiendanube.com>
2024-06-27 21:15:14 -0300

    Explain some akka components

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -7,4 +7,7 @@ if [[ "$@" == "--watch" ]]; then
 fi
 
 mkdir -p target # There is some sort of bug in docker where if the folder does not exist it will reset the ownership in the container to root
-docker run --rm -v $(pwd)/target:/home/tex/app/target -v $(pwd)/src:/home/tex/app/src $LATEX_PROJECT_NAME ${watch:--pvc}
+docker run --rm -v $(pwd)/target:/home/tex/app/target \
+    -v $(pwd)/src:/home/tex/app/src \
+    -v $(pwd)/assets:/home/tex/app/assets \
+    $LATEX_PROJECT_NAME ${watch:--pvc}
